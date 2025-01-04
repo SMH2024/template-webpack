0ce404471c5a8b58e2b188c8b465f59663a3d0bd
SMH2024 <163409341+SMH2024@users.noreply.github.com>
2025-01-04 06:57:53 -0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	new file:   public/assets/00_logsprite.png
    	new file:   public/assets/01_logsprite.png
    	new file:   public/assets/02_logsprite.png
    	new file:   public/assets/03_logsprite.png
    	new file:   public/assets/04_logsprite.png
    	new file:   public/assets/05_logsprite.png
    	new file:   public/assets/06_logsprite.png
    	new file:   public/assets/07_logsprite.png
    	new file:   public/assets/08_logsprite.png
    	new file:   public/assets/09_logsprite.png
    	new file:   public/assets/10_logsprite.png
    	new file:   public/assets/11_logsprite.png
    	new file:   public/assets/12_logsprite.png
    	new file:   public/assets/13_logsprite.png
    	new file:   public/assets/14_logsprite.png
    	new file:   public/assets/15_logsprite.png
    	new file:   public/assets/16_logsprite.png
    	new file:   public/assets/17_logsprite.png
    	new file:   public/assets/18_logsprite.png
    	new file:   public/assets/19_logsprite.png
    	new file:   public/assets/20_logsprite.png
    	new file:   public/assets/21_logsprite.png
    	new file:   public/assets/22_logsprite.png
    	new file:   public/assets/23_logsprite.png
    	new file:   public/assets/24_logsprite.png
    	new file:   public/assets/25_logsprite.png
    	new file:   public/assets/26_logsprite.png
    	new file:   public/assets/27_logsprite.png
    	new file:   public/assets/28_logsprite.png
    	new file:   public/assets/29_logsprite.png
    	new file:   public/assets/30_logsprite.png
    	new file:   public/assets/31_logsprite.png
    	new file:   public/assets/32_logsprite.png
    	new file:   public/assets/33_logsprite.png
    	new file:   public/assets/34_logsprite.png
    	new file:   public/assets/35_logsprite.png
    	new file:   public/assets/36_logsprite.png
    	new file:   public/assets/37_logsprite.png
    	new file:   public/assets/38_logsprite.png
    	new file:   public/assets/39_logsprite.png
    	new file:   public/assets/40_logsprite.png
    	new file:   public/assets/41_logsprite.png
    	new file:   public/assets/42_logsprite.png
    	new file:   public/assets/43_logsprite.png
    	new file:   public/assets/44_logsprite.png
    	new file:   public/assets/copcharacter/body.png
    	new file:   public/assets/copcharacter/head.png
    	new file:   public/assets/copcharacter/left_arm.png
    	new file:   public/assets/copcharacter/left_arm1.png
    	new file:   public/assets/copcharacter/left_forearm.png
    	new file:   public/assets/copcharacter/left_hip.png
    	new file:   public/assets/copcharacter/left_leg.png
    	new file:   public/assets/copcharacter/left_shoulder.png
    	new file:   public/assets/copcharacter/neck.png
    	new file:   public/assets/copcharacter/pants.png
    	new file:   public/assets/copcharacter/right_arm.png
    	new file:   public/assets/copcharacter/right_arm1.png
    	new file:   public/assets/copcharacter/right_forearm.png
    	new file:   public/assets/copcharacter/right_hip.png
    	new file:   public/assets/copcharacter/right_leg.png
    	new file:   public/assets/copcharacter/right_shoulder.png
    	new file:   public/assets/copcharacter/roght_forearm.png
    	new file:   public/assets/copcharacter/shot.png
    	new file:   public/assets/copcharacter/shot0.png
    	new file:   public/assets/copcharacter/shot01.png
    	new file:   public/assets/copcharacter/shot02.png
    	new file:   public/assets/copcharacter/shot03.png
    	new file:   public/assets/copcharacter/shot1.png
    	new file:   public/assets/copcharacter/shot2.png
    	new file:   public/assets/copcharacter/shot3.png
    	new file:   public/assets/copcharacter/truncheon.png
    	new file:   public/assets/copcharacter/weapon.png
    	new file:   public/assets/copcharacter/weapon1.png
    	modified:   src/scenes/Game.js
    	modified:   src/scenes/Preloader.js

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,28 +1,76 @@
 import { Scene } from 'phaser';
 
-export class Game extends Scene
-{
-    constructor ()
-    {
+export class Game extends Scene {
+    constructor() {
         super('Game');
     }
 
-    create ()
-    {
+    create() {
+        // Set background
         this.cameras.main.setBackgroundColor(0x00ff00);
-
         this.add.image(512, 384, 'background').setAlpha(0.5);
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
+        // Add character sprite
+        this.character = this.add.sprite(512, 384, 'logsprite_0').setScale(2);
+
+        // Set default animation
+        this.character.play('walk');
+
+        // Keyboard controls
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.keys = this.input.keyboard.addKeys({
+            block: Phaser.Input.Keyboard.KeyCodes.SPACE,
+            dizzy: Phaser.Input.Keyboard.KeyCodes.D,
+            hit: Phaser.Input.Keyboard.KeyCodes.H,
+        });
+
+        // Display some placeholder text
+        this.add.text(512, 384, 'Use arrow keys to move, SPACE to block,\nD for dizzy, and H to get hit.', {
+            fontFamily: 'Arial Black',
+            fontSize: 24,
+            color: '#ffffff',
+            align: 'center',
         }).setOrigin(0.5);
 
+        // Example scene transition
         this.input.once('pointerdown', () => {
-
             this.scene.start('GameOver');
-
         });
+    }
+
+    update() {
+        const character = this.character;
+
+        // Movement controls
+        if (this.cursors.left.isDown) {
+            character.setFlipX(true);
+            character.x -= 2;
+            character.play('walk', true);
+        } else if (this.cursors.right.isDown) {
+            character.setFlipX(false);
+            character.x += 2;
+            character.play('walk', true);
+        } else if (this.cursors.up.isDown) {
+            character.y -= 2;
+            character.play('walk', true);
+        } else if (this.cursors.down.isDown) {
+            character.y += 2;
+            character.play('walk', true);
+        } else {
+            character.stop();
+        }
+
+        // Action controls
+        if (Phaser.Input.Keyboard.JustDown(this.keys.block)) {
+            character.play('block', true);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.keys.dizzy)) {
+            character.play('dizzy', true);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.keys.hit)) {
+            character.play('hit', true);
+        }
     }
 }
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,46 +1,94 @@
 import { Scene } from 'phaser';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can display it here
+    init() {
         this.add.image(512, 384, 'background');
-
-        //  A simple progress bar. This is the outline of the bar.
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
-
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
-
         });
     }
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
+    preload() {
+        // Set path for assets
         this.load.setPath('assets');
 
+        // Load assets for the game
         this.load.image('logo', 'logo.png');
+        this.load.image('background', 'background.png');
+
+        // Load sprite frames (logsprite_0 to logsprite_44)
+        for (let i = 0; i <= 44; i++) {
+            this.load.image(`logsprite_${i}`, `logsprite_${i.toString().padStart(2, '0')}.png`);
+        }
     }
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
+    create() {
+        // Create animations
+        this.anims.create({
+            key: 'block',
+            frames: [
+                { key: 'logsprite_0' },
+                { key: 'logsprite_1' },
+                { key: 'logsprite_2' },
+                { key: 'logsprite_3' },
+                { key: 'logsprite_4' }
+            ],
+            frameRate: 10,
+            repeat: -1,
+        });
 
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+        this.anims.create({
+            key: 'dizzy',
+            frames: [
+                { key: 'logsprite_5' },
+                { key: 'logsprite_6' },
+                { key: 'logsprite_7' },
+                { key: 'logsprite_8' },
+                { key: 'logsprite_9' }
+            ],
+            frameRate: 8,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'logsprite_10' },
+                { key: 'logsprite_11' },
+                { key: 'logsprite_12' },
+                { key: 'logsprite_13' },
+                { key: 'logsprite_14' }
+            ],
+            frameRate: 12,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'hit',
+            frames: [
+                { key: 'logsprite_15' },
+                { key: 'logsprite_16' },
+                { key: 'logsprite_17' },
+                { key: 'logsprite_18' },
+                { key: 'logsprite_19' },
+                { key: 'logsprite_20' },
+                { key: 'logsprite_21' },
+                { key: 'logsprite_22' },
+                { key: 'logsprite_23' },
+                { key: 'logsprite_24' }
+            ],
+            frameRate: 15,
+            repeat: 0,
+        });
+
+        // Transition to MainMenu scene
         this.scene.start('MainMenu');
     }
 }
